fd2d6053f5dd297efa6e785e08304e575a08269a
oscar <oscar.vasquez@zentro.com.mx>
2023-10-16 16:29:15 -0600

    avances

---
+++ src/componentes/Empleados.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Empleados = () => {
+    console.log("Se ha vuelto a renderizar Empleados!")
+  return (
+    <div>Empleados</div>
+  )
+}
--- src/componentes/Gestion.js
+++ src/componentes/Gestion.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Empleados } from './Empleados'
 
 export const Gestion = () => {
+
+    const [nombre,setNombre] = useState("");
+
+    useEffect(() => {
+        console.log("La vista de Gestion se ha renderizado")
+    })
+
+    const asignarGestor = e =>{
+        setNombre(e.target.value)
+    }
+
   return (
     <div>
-        <h1>Nombre del gestor: Victor</h1>
+        <h1>Nombre del gestor: {nombre}</h1>
+
+        <input type='text' onChange={asignarGestor} placeholder='Introduce tu nombre de gestor'/>
+
+        <h2>Listado de empleados: </h2>
+
+        <p>Los usuarios son gestionados por {nombre} de jsonplaceholder</p>
+
+        <Empleados/>
     </div>
   )
 }
