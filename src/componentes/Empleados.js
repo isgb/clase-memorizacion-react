101322bcd32142e3d88de6c07aa3a021f0a955b9
oscar <oscar.vasquez@zentro.com.mx>
2023-10-17 16:51:04 -0600

    avances

--- src/componentes/Empleados.js
+++ src/componentes/Empleados.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Empleados = () => {
+export const Empleados = React.memo(({ pagina = 1 }) => {
+    
+    const [empleados, setEmpleados] = useState([]);
+    
+    useEffect(() => {
+      conseguirEmpleados(pagina);
+    }, [pagina])
+    
+    const conseguirEmpleados = async (p) => {
+      const url = `https://reqres.in/api/users?page=${p}`;
+      const peticion = await fetch(url);
+      const {data: empleados} = await peticion.json();
+
+      // console.log(empleados)
+      setEmpleados(empleados)
+
+      console.log("Se ejecuto la petición ajax")
+    }
+
     console.log("Se ha vuelto a renderizar Empleados!")
+    
   return (
-    <div>Empleados</div>
+    <div>
+      <p>Mostrando la página : {pagina}</p>
+      <ul className='empleados'>
+        {
+          empleados.length >= 1 && empleados.map(empleado => {
+            return <li key={empleado.id}>{
+              empleado.first_name + " " + empleado.last_name
+            }</li>
+          })
+        }
+      </ul>
+    </div>
   )
 }
+);
--- src/componentes/Gestion.js
+++ src/componentes/Gestion.js
@@ -4,6 +4,7 @@ import { Empleados } from './Empleados'
 export const Gestion = () => {
 
     const [nombre,setNombre] = useState("");
+    const [pagina, setPagina] = useState(1);
 
     useEffect(() => {
         console.log("La vista de Gestion se ha renderizado")
@@ -22,8 +23,10 @@ export const Gestion = () => {
         <h2>Listado de empleados: </h2>
 
         <p>Los usuarios son gestionados por {nombre} de jsonplaceholder</p>
+        <button onClick={() => {setPagina(1)}}>Página 1</button> 
+        <button onClick={() => {setPagina(2)}}>Página 2</button>
 
-        <Empleados/>
+        <Empleados pagina={pagina}/>
     </div>
   )
 }
